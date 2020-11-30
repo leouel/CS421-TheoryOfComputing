c659e185ab2758dfddb3669c9e436235ea0c7ef6
Leouel <leouel.guanzon@gmail.com>
2020-11-30 11:49:17 -0800

    Modified scanner.cpp and updated group23parse.out

--- ParserFiles/scanner.cpp
+++ ParserFiles/scanner.cpp
@@ -267,7 +267,7 @@ int scanner(tokentype& tt, string& w)
 
   if(tt == ERROR)
   {
-	  cout << "\nLexical error: " << w << " is not a valid token." << endl;
+	  cout << "Lexical error: " << w << " is not a valid token." << endl;
   }
 
   return 1;
